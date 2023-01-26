1df8615f59e5151dc8aa6635e6852cfe5c50c38f
Hayden Liles <lileshaydenreal@gmail.com>
2023-01-26 13:59:45 -0700

    did a better rough draft

---
+++ app.js
@@ -0,0 +1,23 @@
+let heroContainer = document.querySelector("#hero-container")
+
+
+let heroArr = [
+  {name: "Andrew", health: 100, attack: 10, level: 0},
+  {name: "Hayden", health: 120, attack: 8, level: 0},
+  {name: "Burt", health: 60, attack: 15, level: 0},
+]
+
+let bossArr = [
+  {name: "Ghosty Boi", health: 100, attack: 5, level: 1}
+]
+
+
+function drawHeros(){
+  let template = ''
+  heroArr.forEach(e => {
+    template += 
+    `
+    
+    `
+  })
+}
