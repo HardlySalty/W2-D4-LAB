d1549c6544dee1146b8adc09be254ae76d359335
Hayden Liles <lileshaydenreal@gmail.com>
2023-01-27 09:09:44 -0700

    fixed the cur health at the start of the game

--- app.js
+++ app.js
@@ -7,29 +7,28 @@ let gold = 0
 mainContainer.addEventListener("click", dealBossDamage)
 
 let heroArr = [
-  {name: "Andrew", alive: true, curHealth: 10, maxHealth: 100, attack: 10, level: 0},
-  {name: "Hayden", alive: true, curHealth: 10, maxHealth: 120, attack: 8, level: 0},
-  {name: "Scott", alive: true, curHealth: 10, maxHealth: 60, attack: 15, level: 0},
+  { name: "Andrew", alive: true, curHealth: 100, maxHealth: 100, attack: 10, level: 0 },
+  { name: "Hayden", alive: true, curHealth: 120, maxHealth: 120, attack: 8, level: 0 },
+  { name: "Scott", alive: true, curHealth: 60, maxHealth: 60, attack: 15, level: 0 },
 ]
 
 let bossArr = [
-  {name: "Ghosty Boi", curHealth: 100, maxHealth: 100, attack: 5, level: 1}
+  { name: "Ghosty Boi", curHealth: 100, maxHealth: 100, attack: 5, level: 1 }
 ]
 
-
-function drawAvatar(){
+function drawAvatar() {
 
   let heroTemplate = ''
   heroArr.forEach(e => {
-    if(e.alive){
-      heroTemplate += 
-      `
+    if (e.alive) {
+      heroTemplate +=
+        `
       <div>
         <p class="text-danger fs-2 text-center fw-bold">${e.curHealth}<span class="text-light fs-5"></span><span class="text-success fs-5">/${e.maxHealth}</span><br>${e.name}</p>
         <img src="./Character1_aseprite_aseprite.gif" class="hero">
       </div>
-      ` 
-    }else{
+      `
+    } else {
       let deadHero = heroArr.findIndex(hero => hero.alive == false)
       heroArr.splice(deadHero, 1)
     }
@@ -38,8 +37,8 @@ function drawAvatar(){
 
   let bossTemplate = ''
   bossArr.forEach(e => {
-    bossTemplate += 
-    `
+    bossTemplate +=
+      `
     <p class="text-danger fs-2 text-center fw-bold ps-5">${e.curHealth}<span class="text-light fs-5"></span><span class="text-success fs-5">/${e.maxHealth}</span><br>${e.name}</p>
     <img src="./Ghosty.gif" height="500" alt="">
     `
@@ -49,15 +48,15 @@ function drawAvatar(){
   heroContainer.innerHTML = heroTemplate
 }
 
-function checkAlive(){
+function checkAlive() {
   heroArr.forEach(e => {
-    if(e.curHealth <= 0){
+    if (e.curHealth <= 0) {
       e.alive = false
     }
   })
 }
 
-function dealBossDamage(){
+function dealBossDamage() {
   let curBoss = bossArr[0]
 
   let damage = 0
@@ -66,7 +65,7 @@ function dealBossDamage(){
   })
 
   curBoss.curHealth -= damage
-  if(curBoss.curHealth <= 0){
+  if (curBoss.curHealth <= 0) {
     drawGold()
     curBoss.level++
     curBoss.attack += 5
@@ -79,29 +78,28 @@ function dealBossDamage(){
   drawAvatar()
 }
 
-function dealHeroDamage(){
+function dealHeroDamage() {
   let curBossAttack = bossArr[0].attack
   let randomNum = Math.floor(Math.random() * heroArr.length)
-  if(heroArr.length > 0){
-    while(heroArr.length-1 > 0 && heroArr[randomNum].curHealth <= 0){
-      randomNum = Math.floor(Math.random() * heroArr.length-1)
+  if (heroArr.length > 0) {
+    while (heroArr.length - 1 > 0 && heroArr[randomNum].curHealth <= 0) {
+      randomNum = Math.floor(Math.random() * heroArr.length - 1)
     }
-  }else{
+  } else {
     window.close()
   }
   heroArr[randomNum].curHealth -= curBossAttack
-  
   drawAvatar()
 }
 
-function drawGold(){
+function drawGold() {
   gold += 100
   goldContainer.innerText = "Gold: " + gold
 }
 
-if(heroArr.length-1 > 0){
-  setInterval(dealHeroDamage, 700)
-}else{
+if (heroArr.length - 1 > 0) {
+  setInterval(dealHeroDamage, 1000)
+} else {
 }
 
 drawAvatar()
